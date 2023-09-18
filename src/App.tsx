f00b2a6fcde84730c621d160d0c46777c62acb73
divyesh2123 <divyesh2198@gmail.com>
2023-09-18 10:12:35 +0530

    this is the tsx changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import data1, {Employee} from './IEmployee';
+
 function App() {
+
+ const [data,setData]= useState<number>(0);
+
+ const changeValue = ()=> {
+
+  setData(data+1);
+ }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {
+      data1.map((value:Employee)=> {
+
+        return(<div>{value.Name}</div>)
+      })
+    }
+
+    <button onClick={changeValue}>+</button>
     </div>
   );
 }
